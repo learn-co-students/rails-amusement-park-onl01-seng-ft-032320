c71ddd2ae78d00357715765a47f47f421a485d4c
antdp425 <antdp425@aol.com>
2020-06-13 00:05:00 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  before_action :logged_in?, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user
+    end
+  end
+
+  def show
+    @user = User.find_by(id: session[:user_id])
+  end
+
+end
+ 
