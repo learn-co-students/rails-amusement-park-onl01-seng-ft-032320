d8c1b4998215c89c17402dfbc180152cae5b2785
proiacm <proiacm@gmail.com>
2020-06-14 22:26:15 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+    
+    root 'application#welcome'
+    resources :users, only: [:new, :create, :show]
+    resources :attractions, only: [:new, :create, :index, :show, :edit, :update]
+    get "signin", to: "sessions#new"
+    post "signin", to: "sessions#create"
+    delete "signout", to: "sessions#destroy"
+    post "/ride/:id", to: "rides#create", as: "new_ride"
 end
