4bc00c5f861dad2244bdf652ec4c95179ba65859
Swishyfishie2 <pclandreiuk@gmail.com>
2020-06-20 22:56:48 +0100

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class User < ActiveRecord::Base
-end
+class User < ApplicationRecord
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+
+    def mood
+       return "sad" if nausea > happiness
+       return "happy" if nausea < happiness
+    end
+
+ end 
