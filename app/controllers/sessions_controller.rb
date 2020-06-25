60a9a82ce1b8d05b4b98615d3e282972a248905b
Beth Ruff <beth.ruff2@gmail.com>
2020-06-25 16:44:44 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        
+        @user = User.find_by(name: params[:user][:name])
+        if @user.authenticate(params[:user][:password])
+            redirect_to signin_path
+        else
+            session[:user_id] = @user.id
+            redirect_to user_path(@user) 
+        end
+    end
+    
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+    end
+
+    # private
+
+    #     def login_params
+    #         params.require(:sessions).permit(:name, :password)
+    #     end 
+end
